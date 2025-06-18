b24d511836574998b79d13022486a2f4e7b4236a
Gao-zi <327186939@qq.com>
2025-06-18 23:45:06 +0800

    测试了own页面

---
+++ src/components/OwnComponents/coma.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        1123
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- src/layout/OwnViewLayout.vue
+++ src/layout/OwnViewLayout.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
         这是我的布局
+        <coma></coma>
     </div>
 </template>
 
 <script setup>
-
+import coma from '@/components/OwnComponents/coma.vue'
 </script>
 
 <style scoped>
