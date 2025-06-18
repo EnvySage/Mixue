e73754a3b2556a362d5e581fde2806f3bba86838
羡笙z <14679730+xiansheng-z@user.noreply.gitee.com>
2025-06-19 01:26:19 +0800

    修改了基础css和添加了主导航

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+import MainNav from './components/MainNav.vue';
 </script>
 
 <template>
 
   <RouterView />
+  <MainNav />
 </template>
 
 <style scoped>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="main-nav">
+        <div 
+            class="detail-box" 
+            v-for="(item, index) in navList" 
+            :key="index"
+            @click="handleNavClick(item.path, index)"
+        >
+            <img :src="isActive(index) ? item.img[1] : item.img[0]" alt="">
+            <div>{{ item.name }}</div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+import mainOff from '@/img/Nav/main-off.png'
+import orderOff from '@/img/Nav/order-off.png'
+import listOff from '@/img/Nav/list-off.png'
+import ownOff from '@/img/Nav/own-off.png'
+import mainOn from '@/img/Nav/main-on.png'
+import orderOn from '@/img/Nav/order-on.png'
+import listOn from '@/img/Nav/list-on.png'
+import ownOn from '@/img/Nav/own-on.png'
+
+const router = useRouter()
+
+const navList = [
+    {
+        path: '/',
+        name: '首页',
+        img: [mainOff, mainOn]
+    },
+    {
+        path: '/order',
+        name: '点餐',
+        img: [orderOff, orderOn]
+    },
+    {
+        path: '/list',
+        name: '订单',
+        img: [listOff, listOn]
+    },
+    {
+        path: '/own',
+        name: '我的',
+        img: [ownOff, ownOn]
+    }
+]
+
+const activeIndex = ref(0) // 默认激活首页
+
+const handleNavClick = (path, index) => {
+    activeIndex.value = index
+    router.push(path)
+}
+
+const isActive = (index) => {
+    return activeIndex.value === index
+}
+</script>
+
+<style scoped>
+.main-nav {
+    background-color: var(--interpret-color);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 10px;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    border-top: 1px solid #ccc;
+}
+.detail-box {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    flex-direction: column;
+    margin-right: 10px;
+}
+.detail-box img {
+    width: 45px;
+    height: 45px;
+}
+.detail-box div {
+    font-size: 16px;
+    color: #554242;
+}
+</style>
