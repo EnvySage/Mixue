fd51637002178f792a45de68a5ef1d27675d89ba
yummaa11 <2848120053@qq.com>
2025-06-19 22:23:09 +0800

    主页测试页面

--- src/layout/MainViewLayout.vue
+++ src/layout/MainViewLayout.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
         这是首页布局
+        <mainTest></mainTest>
     </div>
 </template>
 
 <script setup>
-
+import mainTest from '@/components/MainComponents/mainTest.vue'
 </script>
 
 <style scoped>
