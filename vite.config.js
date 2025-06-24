721c865457f82ff6e92cd0cf77d545f9147ddc11
羡笙z <14679730+xiansheng-z@user.noreply.gitee.com>
2025-06-24 21:57:34 +0800

    配置gitpage

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import { ghPages } from 'vite-plugin-gh-pages'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -9,10 +10,12 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    ghPages(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: '/Mixue/'
 })
